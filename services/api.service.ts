3769661d0a36bcf7e1d48a5a302c8446cc02a837
sony-level <sonydilane@gmail.com>
2023-12-22 13:03:26 +0100

    four

--- services/api.service.ts
+++ services/api.service.ts
@@ -8,34 +8,71 @@ import { Observable } from 'rxjs';
 })
 export class APIService {
 
-  ApiUrl = 'http://swapi.dev/api/';
+  private ApiUrl = 'http://swapi.dev/api/';
 
 
   constructor(private http: HttpClient) { }
 
 
-  getFilms(): Observable<any> {
+ 
+  getAll(): Observable<any> {
+    return this.http.get(`${this.ApiUrl}`);
+ }
+
+ getFilms(): Observable<any> {
     return this.http.get<any>(`${this.ApiUrl}films/`);
-  }
+ }
 
-  getPeople(): Observable<any> {
+ getFilm(id: number): Observable<any> {
+    return this.http.get(`${this.ApiUrl}/films/${id}/`);
+ }
+
+ search(searchTerm: string, type?: string): Observable<any> {
+    let url = `${this.ApiUrl}?search=${searchTerm}&format=json`;
+    return this.http.get<any>(url);
+ }
+
+ getPeople(): Observable<any> {
     return this.http.get<any>(`${this.ApiUrl}people/`);
-  }
+ }
 
-  getPlanets(): Observable<any> {
-    return this.http.get<any>(`${this.ApiUrl}planets/`);
-  }
+ getPerson(id: number): Observable<any> {
+    return this.http.get(`${this.ApiUrl}/people/${id}/`);
+ }
 
-  getSpecies(): Observable<any> {
-    return this.http.get<any>(`${this.ApiUrl}species/`);
-  }
+ addPerson(person: any): Observable<any> {
+    const body = JSON.stringify(person);
+    return this.http.post('https://us-central1-lab4-85e63.cloudfunctions.net', body);
+ }
 
-  getVehicles(): Observable<any> {
+ getPlanets(id: number): Observable<any> {
+    return this.http.get(`${this.ApiUrl}/planets/${id}/`);
+ }
+
+ getSpecies(id: number): Observable<any> {
+    return this.http.get(`${this.ApiUrl}/species/${id}/`);
+ }
+
+ saveDataToDB(data: any): void {
+    console.log("Saving data in DB");
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:9000/saveData", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+        alert("Data saved!");
+      } else {
+        alert("Error while uploading file:\n" + xhr.responseText);
+      }
+    };
+    xhr.send(JSON.stringify(data));
+ }
+
+ getVehicles(): Observable<any> {
     return this.http.get<any>(`${this.ApiUrl}vehicles/`);
-  }
+ }
 
-  getStarships(): Observable<any> {
+ getStarships(): Observable<any> {
     return this.http.get<any>(`${this.ApiUrl}starships/`);
-  }
-
+ }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,53 @@ import { UntypedFormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-  title = 'Movies-v1';
-  films: any[] = [];
-
-  constructor(private apiService: APIService ) { }
-
-  ngOnInit(): void {
-    this.apiService.getFilms().subscribe((data: any) => {
-      this.films = data.results;
-    });
+  title = 'my-swapi-project';
+  films: any;
+  people: any;
+  planets: any;
+  starships: any;
+  vehicles: any;
+  species: any;
+  data: any;
+ 
+  constructor(private Service: APIService) {
+     this.getAllData();
   }
-​
+ 
+  getAllData() {
+     this.Service.getFilms().subscribe(data => {
+       console.log('Films:', data);
+       this.films = data.results; // Fix: Assign the results to the films variable
+     });
+ 
+     this.Service.getPeople().subscribe(data => {
+       console.log('People:', data);
+       this.people = data.results; // Fix: Assign the results to the people variable
+     });
+ 
+     this.Service.getPlanets(1).subscribe(data => {
+       console.log('Planets:', data);
+       this.planets = data.results; // Fix: Assign the results to the planets variable
+     });
+ 
+     this.Service.getStarships().subscribe(data => {
+       console.log('starships:', data);
+       this.starships = data.results; // Fix: Assign the results to the starships variable
+     });
+ 
+     this.Service.getVehicles().subscribe(data => {
+       console.log('Vehicles:', data);
+       this.vehicles = data.results; // Fix: Assign the results to the vehicles variable
+     });
+ 
+     this.Service.getSpecies(1).subscribe(data => {
+       console.log('Species:', data);
+       this.species = data.results; // Fix: Assign the results to the species variable
+     });
+  }
+ 
+  ngOnInit() {
+     this.getAllData();
+  }
 
 }
