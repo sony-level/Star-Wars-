d3f12acbd2be782e7689fa55849e49ba8412840c
sony-level <sonydilane@gmail.com>
2023-12-30 02:48:46 +0100

    maque l'affichages des connections propre a chaque personnage

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -11,6 +11,11 @@ export class PeopleComponent implements OnInit{
   
   peopleList: any[] = [];
   currentPage: number = 1;
+  personFilms: any[] = [];
+  personStarships: any[] = [];
+  personVehicles: any[] = [];
+  personSpecies: any[] = [];
+  selectedPerson: any;
 
   constructor(private apiService: APIService) { }
 
@@ -30,6 +35,64 @@ export class PeopleComponent implements OnInit{
     );
   }
 
+  getPersonDetails(type: string, urls: string[]): void {
+    const personDetails: any[] = [];
+  
+    if (urls && urls.length > 0) {
+      urls.forEach((url: string) => {
+        this.apiService.getData(url).subscribe((data) => {
+          personDetails.push(data);
+  
+          if (personDetails.length === urls.length) {
+            this.assignPersonDetails(type, personDetails);
+          }
+        });
+      });
+    } else {
+      // Aucune donnée disponible pour cette personne
+      this.assignPersonDetails(type, []);
+    }
+  }
+  
+  assignPersonDetails(type: string, details: any[]): void {
+    switch (type) {
+      case 'films':
+        this.personFilms = details;
+        break;
+      case 'starships':
+        this.personStarships = details;
+        break;
+      case 'vehicles':
+        this.personVehicles = details;
+        break;
+      case 'species':
+        this.personSpecies = details;
+        break;
+      // Ajoutez d'autres cas au besoin
+      default:
+        break;
+    }
+  }
+
+  showPersonDetailsByFilm(film: any) {
+    // Réinitialisez les détails des personnes
+    this.personFilms = [];
+    this.personStarships = [];
+    this.personVehicles = [];
+    this.personSpecies = [];
+  
+    // Parcourez la liste des personnes
+    for (const person of this.peopleList) {
+      // Vérifiez si la personne est présente dans les characters du film
+      if (film.characters.includes(person.url)) {
+        // Chargez les détails des connexions pour cette personne
+        this.getPersonDetails('films', person.films);
+        this.getPersonDetails('starships', person.starships);
+        this.getPersonDetails('vehicles', person.vehicles);
+        this.getPersonDetails('species', person.species);
+      }
+    }
+  }
   nextPage(): void {
     this.currentPage++;
     this.loadPeople();
